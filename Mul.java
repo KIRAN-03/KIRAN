0fa7369d4a216839a9867566f1d968d419225cf6
KIRAN-03 <121012948+KIRAN-03@users.noreply.github.com>
2022-12-19 14:28:12 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Mul
+{
+ public static void main(String args[])
+ {
+  int i,j;
+  System.out.println("enter 2 mul values");
+  Scanner s=new Scanner(System.in);
+  i=s.nextInt();
+  j=s.nextInt();
+  System.out.println(i+"*"+j+"="+(i*j));
+ }
+}
